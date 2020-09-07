59eaccd6bd2d77d00ab73c125805f2ed707d3d83
ffd <ffdelacruz@icloud.com>
2020-09-07 11:50:02 +0800

    Add LandingVC

---
+++ CartrackContacts/Views/Landing/LandingViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LandingViewController.swift
+//  CartrackContacts
+//
+//  Created by Feb De La Cruz on 9/7/20.
+//  Copyright © 2020 Feb De La Cruz. All rights reserved.
+//
+
+import UIKit
+
+class LandingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
