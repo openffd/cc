ec6c17907c1d64320d0ca26cc09f10e2005f1fa4
ffd <ffdelacruz@icloud.com>
2020-09-07 11:59:53 +0800

    Create new group Extensions; Add UIStoryboard extension

---
+++ CartrackContacts/Extensions/UIStoryboard+.swift
@@ -0,0 +1,14 @@
+//
+//  UIStoryboard+.swift
+//  CartrackContacts
+//
+//  Created by Feb De La Cruz on 9/7/20.
+//  Copyright © 2020 Feb De La Cruz. All rights reserved.
+//
+
+import UIKit
+
+extension UIStoryboard {
+    static let landing  = UIStoryboard(name: "Landing", bundle: .main)
+    static let home     = UIStoryboard(name: "Home", bundle: .main)
+}
--- CartrackContacts/SceneDelegate.swift
+++ CartrackContacts/SceneDelegate.swift
@@ -17,8 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        let storyboard = UIStoryboard(name: "Landing", bundle: .main)
-        let viewController = storyboard.instantiateInitialViewController()
+        let viewController = UIStoryboard.landing.instantiateInitialViewController()
         window.rootViewController = viewController
         window.makeKeyAndVisible()
         self.window = window
