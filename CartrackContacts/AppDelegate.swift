ab994b0bab88b030f807c57dacdf1ea2396cde32
ffd <ffdelacruz@icloud.com>
2020-09-07 16:29:22 +0800

    Create Launch storyboard initial UI

--- CartrackContacts/AppDelegate.swift
+++ CartrackContacts/AppDelegate.swift
@@ -12,6 +12,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        Thread.sleep(forTimeInterval: 1.25)
         initializeSharedDatabase()
         return true
     }
