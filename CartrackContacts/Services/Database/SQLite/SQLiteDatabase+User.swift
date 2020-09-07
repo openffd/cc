540dfbd122a637fb7d98be4186d50909484b6ae2
ffd <ffdelacruz@icloud.com>
2020-09-07 15:21:31 +0800

    Implement SQLite.Database#insertUser

--- CartrackContacts/Services/Database/SQLite/SQLiteDatabase+User.swift
+++ CartrackContacts/Services/Database/SQLite/SQLiteDatabase+User.swift
@@ -15,7 +15,18 @@ extension SQLite.Database: UserDatabase {
     }
     
     func insertUser(user: User) throws {
-        
+        let queryString = "INSERT INTO user (id, username, digest, country) VALUES (NULL, ?, ?, ?);"
+        let statementPointer = try prepareStatement(query: queryString)
+        defer { sqlite3_finalize(statementPointer) }
+        guard
+            sqlite3_bind_text(statementPointer, 1, user.username.utf8, -1, nil) == SQLITE_OK &&
+            sqlite3_bind_text(statementPointer, 2, user.digest.utf8, -1, nil) == SQLITE_OK &&
+            sqlite3_bind_text(statementPointer, 3, user.country.utf8, -1, nil) == SQLITE_OK else {
+            throw SQLite.Error.bind(message: recentErrorMessage)
+        }
+        guard sqlite3_step(statementPointer) == SQLITE_DONE else {
+            throw SQLite.Error.step(message: recentErrorMessage)
+        }
     }
     
     func getUser(username: String) -> User? {
