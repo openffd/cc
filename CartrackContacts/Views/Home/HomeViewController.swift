533b773621d282dbbaa1d765e3e61723eb71e229
ffd <ffdelacruz@icloud.com>
2020-09-07 11:54:33 +0800

    Add Home storyboard and VC; Add Reachability via SPM

---
+++ CartrackContacts/Views/Home/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  CartrackContacts
+//
+//  Created by Feb De La Cruz on 9/7/20.
+//  Copyright © 2020 Feb De La Cruz. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
