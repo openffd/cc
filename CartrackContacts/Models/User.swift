25a71e6a18a90e79ab47bc3729d6be2d5132347a
ffd <ffdelacruz@icloud.com>
2020-09-07 12:24:22 +0800

    Add User model; Add new Model group Contacts

---
+++ CartrackContacts/Models/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  CartrackContacts
+//
+//  Created by Feb De La Cruz on 9/7/20.
+//  Copyright © 2020 Feb De La Cruz. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+    let id: Int
+    let username: String
+    let digest: String
+    let country: String
+}
