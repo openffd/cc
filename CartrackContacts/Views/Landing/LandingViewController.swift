4c77daee6d3c991c7e7fb2c7c9b660a7cacce256
ffd <ffdelacruz@icloud.com>
2020-09-08 20:06:37 +0800

    Add Sedan animation; Initial implementation of LandingVC UI

--- CartrackContacts/Views/Landing/LandingViewController.swift
+++ CartrackContacts/Views/Landing/LandingViewController.swift
@@ -7,10 +7,33 @@
 //
 
 import UIKit
+import Lottie
 
 class LandingViewController: UIViewController {
 
+    @IBOutlet private var animationContainerView: UIView! {
+        didSet {
+            animationContainerView.backgroundColor = .clear
+        }
+    }
+    
+    private var animationView: AnimationView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = .systemOrange
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        animationView = .init(name: "Sedan")
+        animationView?.frame = animationContainerView.bounds
+        animationView?.backgroundColor = .clear
+        animationView?.contentMode = .scaleAspectFill
+        animationView?.loopMode = .loop
+        animationContainerView.addSubview(animationView!)
+        animationView?.play()
     }
 }
