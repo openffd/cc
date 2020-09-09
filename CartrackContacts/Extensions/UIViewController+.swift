539fcd92ac3ea3c8f56d964be1b29cb8c65eefce
ffd <ffdelacruz@icloud.com>
2020-09-09 10:32:20 +0800

    Add UIViewController extension for presenting alerts

---
+++ CartrackContacts/Extensions/UIViewController+.swift
@@ -0,0 +1,31 @@
+//
+//  UIViewController+.swift
+//  CartrackContacts
+//
+//  Created by Feb De La Cruz on 9/9/20.
+//  Copyright © 2020 Feb De La Cruz. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    func presentAlert(for error: Error) {
+        let alertController = UIAlertController(
+            title: "Error",
+            message: error.localizedDescription,
+            preferredStyle: .alert
+        )
+        alertController.addAction(.init(title: "Okay", style: .default))
+        present(alertController, animated: true)
+    }
+    
+    func presentAlert(for message: String) {
+        let alertController = UIAlertController(
+            title: "Message",
+            message: message,
+            preferredStyle: .alert
+        )
+        alertController.addAction(.init(title: "Okay", style: .default))
+        present(alertController, animated: true)
+    }
+}
