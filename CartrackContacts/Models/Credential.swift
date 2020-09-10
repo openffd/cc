d9582c6d71741b088d100267660bba389241934c
ffd <ffdelacruz@icloud.com>
2020-09-10 11:43:42 +0800

    Create Credential struct and protocols supporting login validation

---
+++ CartrackContacts/Models/Credential.swift
@@ -0,0 +1,28 @@
+//
+//  Credential.swift
+//  CartrackContacts
+//
+//  Created by Feb De La Cruz on 9/10/20.
+//  Copyright © 2020 Feb De La Cruz. All rights reserved.
+//
+
+import Foundation
+
+protocol LoginCredential {
+    var username: String { get }
+    var password: String { get }
+}
+
+struct Credential: LoginCredential {
+    let username: String
+    let password: String
+}
+
+enum LoginCredentialValidation {
+    case matched
+    case notMatched
+}
+
+protocol LoginCredentialValidator {
+    func validateCredential(_ credential: LoginCredential) -> LoginCredentialValidation
+}
