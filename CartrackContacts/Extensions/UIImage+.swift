7e977ff2807d807c8ee81c2d85f245968f2a0956
ffd <ffdelacruz@icloud.com>
2020-09-11 00:28:37 +0800

    Add UIImage extension for static image constants

---
+++ CartrackContacts/Extensions/UIImage+.swift
@@ -0,0 +1,14 @@
+//
+//  UIImage+.swift
+//  CartrackContacts
+//
+//  Created by Feb De La Cruz on 9/10/20.
+//  Copyright © 2020 Feb De La Cruz. All rights reserved.
+//
+
+import UIKit
+
+extension UIImage {
+    static let passwordHidden   = UIImage(named: "PasswordHidden")!
+    static let passwordVisible  = UIImage(named: "PasswordVisible")!
+}
