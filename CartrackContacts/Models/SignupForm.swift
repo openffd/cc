ce0f4cb74ff0a2d92baf3fb741c988f31ff2d5b6
ffd <ffdelacruz@icloud.com>
2020-09-13 11:08:35 +0800

    Create SignupForm struct

---
+++ CartrackContacts/Models/SignupForm.swift
@@ -0,0 +1,15 @@
+//
+//  SignupForm.swift
+//  CartrackContacts
+//
+//  Created by Feb De La Cruz on 9/13/20.
+//  Copyright © 2020 Feb De La Cruz. All rights reserved.
+//
+
+import Foundation
+
+struct SignupForm {
+    let username: String
+    let password: String
+    let country: String
+}
