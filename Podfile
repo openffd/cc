df674f40865956d8115f1748adcabc4af1292c96
ffd <ffdelacruz@icloud.com>
2020-09-20 23:50:24 +0800

    Install PubNub via CocoaPods

--- Podfile
+++ Podfile
@@ -4,6 +4,7 @@ target 'CartrackContacts' do
   use_frameworks!
 
   pod 'CountryPickerView'
+  pod 'PubNub', '~> 4.6'
 
   target 'CartrackContactsTests' do
     inherit! :search_paths
--- Pods/Target Support Files/Pods-CartrackContacts/Pods-CartrackContacts-frameworks.sh
+++ Pods/Target Support Files/Pods-CartrackContacts/Pods-CartrackContacts-frameworks.sh
@@ -198,9 +198,11 @@ fi
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/CountryPickerView/CountryPickerView.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/PubNub/PubNub.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/CountryPickerView/CountryPickerView.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/PubNub/PubNub.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
