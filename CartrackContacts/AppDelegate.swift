29880343723df8aa770db3ca4ab86ac0d8d7b5e3
ffd <ffdelacruz@icloud.com>
2020-09-21 18:07:24 +0800

    Remove sleep call on AppDelegate

--- CartrackContacts/AppDelegate.swift
+++ CartrackContacts/AppDelegate.swift
@@ -12,7 +12,6 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        Thread.sleep(forTimeInterval: 0.25)
         initializeSharedDatabase()
         return true
     }
